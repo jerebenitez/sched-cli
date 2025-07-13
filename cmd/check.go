0343e48444201591f105e84cfad1f2f73e2413bc
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-07-13 13:14:15 -0300

    chore: traverse directory recursively

--- cmd/check.go
+++ cmd/check.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -16,9 +19,36 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("check called")
-	},
+	Run: checkImpl,
+}
+
+func checkImpl(cmd *cobra.Command, args []string) {
+	// Read files from src/ and orig/
+	readRecursiveDir(".", 0)
+}
+
+func readRecursiveDir(name string, level int) {
+	files, err := os.ReadDir(name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fileName := name + "/" + file.Name()
+		fmt.Println(strings.Repeat(" ", level * 2) + file.Name())
+		fi, err := os.Lstat(fileName)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch mode := fi.Mode(); {
+		case mode.IsDir(): 
+			readRecursiveDir(fileName, level + 1)
+		}
+	}
+
 }
 
 func init() {
