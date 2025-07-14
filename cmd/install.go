dcd4ed93c89672d3f2dd95033c9b2a2a5c3472ae
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-07-14 18:29:36 -0300

    chore: untested install command

--- cmd/install.go
+++ cmd/install.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/jerebenitez/sched-cli/lib"
 	"github.com/spf13/cobra"
 )
 
@@ -16,9 +21,52 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
-	},
+	Run: runInstall,
+}
+
+func runInstall(cmd *cobra.Command, args []string) {
+	src := rootCmd.PersistentFlags().Lookup("src").Value.String()
+	src = strings.Trim(src, "/")
+	dir := rootCmd.PersistentFlags().Lookup("dir").Value.String()
+	dir = strings.Trim(dir, "/")
+
+	fmt.Printf("Installing files to %s...\n", src)
+
+	files := lib.ReadRecursiveDir(dir, "src")
+
+	for _, file := range files {
+		path := lib.GetPath(file)
+		if err := os.MkdirAll(path, os.ModePerm); err != nil {
+			log.Fatalf("Error creating folder %s!. Quitting.\n", path)
+			return
+		}
+
+		// TODO: Test that this behaves nicely when compiling the kernel
+		err := os.Link(filepath.Join(dir, file), filepath.Join(src, file))
+		if err != nil {
+			log.Fatalf("Error copying file %s!. Quitting.\n", file)
+		}
+
+	}
+	
+	fmt.Println("Applying patches...")
+
+	patches := lib.ReadRecursiveDir(dir, "patches")
+
+	for _, patch := range patches {
+		result, err := lib.ApplyPatch(
+			filepath.Join(src, lib.TrimExtension(patch)),
+			filepath.Join(dir, patch),
+			true,
+		)
+		if err != nil {
+			log.Fatalf("Unable to patch %s: %s\n", lib.TrimExtension(patch), err)
+		} else if !result {
+			log.Fatalf("Unable to patch %s: file not compatible with patch. Run update first!\n", lib.TrimExtension(patch))
+		}
+	}
+
+	fmt.Println("Installation completed. You may now compile and install the kernel.")
 }
 
 func init() {
