c39346ac5569a38d8181527f36989807cff51cfd
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-07-17 23:42:37 -0300

    chore: refactored generate-diff to make it more testeable
    I still need to fix some things for it to work

--- cmd/generateDiff.go
+++ cmd/generateDiff.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -12,70 +13,114 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type generateConfig struct {
+	FilePath string
+	Original string
+	Dir		 string
+	Src		 string
+	Out		 io.Writer
+}
+
+//TODO: Fix helpers to get file from repo/folder
 // generateDiffCmd represents the generateDiff command
 var generateDiffCmd = &cobra.Command{
 	Use:   "generate-diff [path to modified file]",
+	Aliases: []string{"gd"},
 	Short: "Generate a patch for a modified file in the source tree:",
 	Long: `Generate a patch for a modified file in the source tree:
   - If the source tree is a git repo, the original version is taken from HEAD.
   - If not, the path to a backup must be provided through --original (-o).`,
   	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		runGenerateDiff(args[0])
+		original, err := cmd.Root().PersistentFlags().GetString("original")
+		if err != nil {
+			log.Fatalf("could not read --original: %v", err)
+		}
+
+		dir, err := cmd.Root().PersistentFlags().GetString("dir")
+		if err != nil {
+			log.Fatalf("could not read --dir: %v", err)
+		}
+
+		src, err := cmd.Root().PersistentFlags().GetString("src")
+		if err != nil {
+			log.Fatalf("could not read --src: %v", err)
+		}
+
+		filePath := args[0]
+
+		fmt.Printf("Generating diff for %s...\n", filePath)
+		err = runGenerateDiff(generateConfig{
+			FilePath: filePath,
+			Original: original,
+			Dir:	  dir,
+			Src:	  src,
+			Out:	  os.Stdout,
+		})
+
+		if err != nil {
+			log.Fatalf("generate-diff failed: %v", err)
+		}
 	},
 }
 
-func runGenerateDiff(patchPath string) {
-	// TODO: Refactor this function
-	// TODO: Get path from --src
-	// TODO: Test function
-	fmt.Printf("Generating diff for %s...\n", patchPath)
+func runGenerateDiff(cfg generateConfig) error {
+	path, _ := filepath.Split(cfg.FilePath)
+	fullPath := filepath.Join(cfg.Dir, path)
 
-	path, _ := filepath.Split(patchPath)
-	if err := os.MkdirAll(path, os.ModePerm); err != nil {
-		log.Fatalf("Error creating folders!. Quitting.\n")
+	err := os.MkdirAll(fullPath, os.ModePerm)
+	if err != nil {
+		return err
 	}
 
 	var originalContent []byte
-
-	if lib.IsGitRepo(path) {
-		cmd := exec.Command("git", "show", fmt.Sprintf("HEAD:%s", path))
-		cmd.Dir = path
-		out, err := cmd.Output()
-		if err != nil {
-			log.Fatalf("Error reading original from git: %v", err)
-			return
-		}
-		originalContent = out
+	if lib.IsGitRepo(cfg.Src) {
+		originalContent, err = getFromRepo(cfg.Src)	
 	} else {
-		originalPath := rootCmd.PersistentFlags().Lookup("original").Value.String()
-		if originalPath == "" {
-			log.Fatalf("Folder is not a git repository, need to provide path to original with --original.")
-			return
-		}
+		originalContent, err = getFromOriginal(path, cfg.Original)
+	}
 
-		originalFullPath := filepath.Join(originalPath, path)
-		content, err := os.ReadFile(originalFullPath)
-		if err != nil {
-			log.Fatalf("Cannot read original file from %s: %v", originalFullPath, err)
-			return
-		}
-		originalContent = content
+	if err != nil {
+		return err
 	}
 
 	newContent, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalf("Cannot read current file: %v", err)
-		return
+		return err
 	}
 
 	patch := godiffpatch.GeneratePatch(path, string(originalContent), string(newContent))
-	if err := os.WriteFile(patchPath, []byte(patch), 0644); err != nil {
-		log.Fatalf("Cannot write patch to %s: %v", patchPath, err)
-		return
+	if err := os.WriteFile(fullPath, []byte(patch), 0644); err != nil {
+		return err
 	}
 
-	fmt.Println("Diff generated successfully.")
+	fmt.Fprintf(cfg.Out, "Diff generated successfully.\n")
+	return nil
+}
+
+func getFromRepo(path string) ([]byte, error) {
+	cmd := exec.Command("git", "show", fmt.Sprintf("HEAD:%s", path))
+	cmd.Dir = path
+
+	out, err := cmd.Output()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return out, nil
+}
+
+func getFromOriginal(path, original string) ([]byte, error) {
+	if original == "" {
+		return []byte{}, fmt.Errorf("folder is not a git repository, need to provide path to original with --original")
+	}
+
+	originalPath := filepath.Join(original, path)
+	content, err := os.ReadFile(originalPath)
+	if err != nil {
+		return []byte{}, err
+	}
+	return content, nil
 }
 
 func init() {
